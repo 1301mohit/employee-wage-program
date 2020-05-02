aec1c9c419d45be57d560221f79d8263988bbf55
1301mohit <mohitkr1301@gmail.com>
2020-05-02 12:42:40 +0530

    ADDED: Function of employee attendance

--- EmployeeWageProgram.sh
+++ EmployeeWageProgram.sh
@@ -1,2 +1,20 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Comuptation Program"
+
+function employeeAttendance() {
+	local present=1;
+	local checkRandom=$((RANDOM%2));
+	if [ $checkRandom -eq $present ]
+	then
+		echo "Employee is present";
+	else
+		echo "Employee is absent";
+	fi
+}
+
+function main() {
+	local checkAttendance="$(employeeAttendance)";
+	echo $checkAttendance;
+}
+
+main
